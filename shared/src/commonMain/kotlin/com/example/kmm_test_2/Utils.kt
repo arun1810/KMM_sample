eafa465cdf0245b0f311e50cd1cf97b02cb54bd5
arun1810 <arunscs35@gmail.com>
2023-08-02 13:57:28 +0530

    comments

--- shared/src/commonMain/kotlin/com/example/kmm_test_2/Utils.kt
+++ shared/src/commonMain/kotlin/com/example/kmm_test_2/Utils.kt
@@ -3,4 +3,4 @@ package com.example.kmm_test_2
     /**
      * we need UUID from this function, for android we can use UUID, but for ios this is not available we need separate implementation for that, so this approach
      */
-    expect fun getUUID():String
+    expect fun getUUID():String // same signature is very important for this implementation.
