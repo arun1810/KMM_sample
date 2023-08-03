311bf5fc09dd7dbf92b4393968b75d3d118b506f
arun1810 <arunscs35@gmail.com>
2023-08-03 15:30:43 +0530

    compose version updated

--- androidKMMApp/build.gradle.kts
+++ androidKMMApp/build.gradle.kts
@@ -38,12 +38,13 @@ android {
     }
 }
 
+val composeVersion ="1.4.3"
 dependencies {
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling:1.4.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
-    implementation("androidx.compose.foundation:foundation:1.4.3")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
     implementation("androidx.compose.material:material:1.4.3")
-    implementation("androidx.activity:activity-compose:1.7.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
 }
